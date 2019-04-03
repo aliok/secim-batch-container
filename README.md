bf3f8e285c7069cec1aa256ac7a9a8b7a017ba9a
Ali Ok <aliok@apache.org>
2019-04-03 14:17:27 +0300

    Initial import

---
+++ README.md
@@ -0,0 +1,61 @@
+### Kurulum:
+
+```
+npm install
+```
+
+### Baslatma:
+
+```
+BATCH_INDEX=0 BATCH_SIZE=5 npm start
+```
+
+### Endpointler:
+
+- <localhost:3000/status>: `RUNNING` veya `DONE` olabilir.
+
+- <localhost:3000/result>: Islem bittiginde, yani `status` `DONE` oldugunda, buradan verdiginiz batch icin sonuclari alabilirsiniz. Ornegin:
+
+```json
+{
+  "&#x130;STANBUL/ADALAR/1006": {
+    "A": {
+      "votes": {
+        "txtCHP": "131",
+        "txtAKP": "66",
+        "txtSAADET": "0",
+        "txtDSP": "30"
+      },
+      "info": {
+        "tbMvKayitliSecmenSayisi": "341",
+        "tbMvOyKullananKayitliSecmenSayisi": "236",
+        "tbMvKanunGeregiOyKullananSayisi": "0",
+        "tbMvKullanilanToplamOy": "236",
+        "tbMvItirazsizGecerliOySayisi": "0",
+        "tbMvItirazliGecerliOySayisi": "0",
+        "tbMvGecerliOySayisi": "227",
+        "tbMvGecersizOySayisi": "9",
+        "lblIlIlceBaslik": "&#x130;STANBUL/ADALAR/1006",
+        "lblMvOzetSandikAlani": "H&#xDC;SEY&#x130;N RAHM&#x130; G&#xDC;RPINAR L&#x130;SES&#x130;",
+        "lblMvGelisZamani": "31.03.2019 23:53:26"
+      }
+    },
+    ...
+}
+```
+
+- <localhost:3000/errors>: Alinamayan/islenemeyen sandik bilgilerini verir.
+
+### Docker
+
+Once image olusturalim:
+
+```
+docker build . -t aliok/secim-batch-container
+```
+
+Calistiralim:
+
+```
+docker run -e BATCH_SIZE=5 -e BATCH_INDEX=10 -p 3000:3000 aliok/secim-batch-container
+```
---
+++ index.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const {PendingXHR} = require('pending-xhr-puppeteer');
+const cheerio = require('cheerio');
+
+const app = express();
+const port = 3000;
+
+const boxUrls = require('./boxUrls.json');
+
+let status = 'RUNNING';
+let result = {};
+let errors = [];
+
+app.get('/status', function (req, res) {
+    res.send(status);
+});
+
+app.get('/result', function (req, res) {
+    res.send(result);
+});
+
+app.get('/errors', function (req, res) {
+    res.send(errors);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+console.log('Environment:');
+console.log('--BATCH_INDEX', process.env['BATCH_INDEX']);
+console.log('--BATCH_INDEX', process.env['BATCH_SIZE']);
+console.log('--CHROME_EXECUTABLE_PATH', process.env['CHROME_EXECUTABLE_PATH']);
+const batchSize = parseInt(process.env['BATCH_SIZE'], 10);
+const batchIndex = parseInt(process.env['BATCH_INDEX'], 10);
+const chromeExecutablePath = process.env['CHROME_EXECUTABLE_PATH'];
+
+const boxCount = Object.keys(boxUrls).length;
+const startIndex = batchSize * batchIndex;
+const endIndex = Math.min(startIndex + batchSize, boxCount);
+
+if (startIndex <= endIndex) {
+    startBatchProcess(startIndex, endIndex);
+} else {
+    status = 'DONE'
+}
+
+function startBatchProcess(startIndex, endIndex) {
+    let sandikCount = endIndex - startIndex + 1;
+    console.log('Start and end indices', startIndex, endIndex);
+
+    const boxNames = Object.keys(boxUrls);
+    const boxNamesToProcess = boxNames.slice(startIndex, endIndex);
+
+    (async () => {
+        const browser = await puppeteer.launch({
+            executablePath: chromeExecutablePath || undefined,
+            headless: true, args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-accelerated-2d-canvas',
+                '--disable-gpu',
+                '--disable-dev-shm-usage'
+            ]
+        });
+        let counter = 0;
+        for (let boxName of boxNamesToProcess) {
+            counter++;
+            console.log(new Date(), 'Reading box: ' + boxName + '      ' + counter + '/' + sandikCount);
+            try {
+                const boxResults = await getBoxResults(browser, boxName, boxUrls[boxName]);
+                console.log('Successfully fetched box results for box', boxName);
+                result[boxName] = boxResults;
+            } catch (ex) {
+                console.log('Error fetching box results for box', boxName);
+                console.log(ex);
+                errors.push(boxName);
+            }
+        }
+        await browser.close();
+
+        status = 'DONE';
+    })();
+}
+
+
+async function getBoxResults(browser, name, url) {
+    console.log('Going to read box results for', name, url);
+
+    const boxResults = {};
+
+    const page = await browser.newPage();
+    const pendingXHR = new PendingXHR(page);
+
+    await page.setRequestInterception(true);
+
+    page.on('request', (req) => {
+        if (req.resourceType() === 'stylesheet' || req.resourceType() === 'font' || req.resourceType() === 'image') {
+            req.abort();
+        }
+        else {
+            req.continue();
+        }
+    });
+
+    await page.goto('https://sts.chp.org.tr/SonucDetay.aspx?cmd=' + url, {waitUntil: 'networkidle2'});
+    await page.waitForSelector('.chp-vote-row');
+    boxResults['A'] = parseBoxPageHtml(await page.content());
+
+
+    await page.evaluate(() => {
+        return document.querySelector('#btn3').click();
+    });
+    await pendingXHR.waitForAllXhrFinished();
+    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+    await page.waitFor(100);
+    boxResults['B'] = parseBoxPageHtml(await page.content());
+
+
+    await page.evaluate(() => {
+        document.querySelector('#btn6').click();
+    });
+    await pendingXHR.waitForAllXhrFinished();
+    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+    await page.waitFor(100);
+    boxResults['C'] = parseBoxPageHtml(await page.content());
+
+    await page.close();
+
+    return boxResults;
+}
+
+function parseBoxPageHtml(content) {
+    const $ = cheerio.load(content);
+    const voteRows = $('.chp-vote-fields-container').find('.chp-vote-row');
+    const votes = {};
+    voteRows.each(function () {
+        const input = $(this).find('input');
+        votes[input.attr('name')] = input.val();
+    });
+
+    const infoRows = $('.chp-vote-fields-container .chp-crate-info').find('.chp-info-row');
+    const info = {};
+    infoRows.each(function () {
+        const input = $(this).find('input');
+        info[input.attr('name')] = input.val();
+    });
+
+    info['lblIlIlceBaslik'] = $('#lblIlIlceBaslik').html();
+    info['lblMvOzetSandikAlani'] = $('#lblMvOzetSandikAlani').html();
+    info['lblMvGelisZamani'] = $('#lblMvGelisZamani').html();
+
+    return {votes: votes, info: info};
+}
+
